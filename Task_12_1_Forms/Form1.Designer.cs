13937b3e2de424c6d8041778b768c44f8a047ecc
SaveLuciferR <cmudrec@mail.ru>
2022-12-22 13:02:11 +0300

    Добавьте файлы проекта.

---
+++ Task_12_1_Forms/Form1.Designer.cs
@@ -0,0 +1,417 @@
+﻿namespace Task_12_1_Forms
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		/// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Код, автоматически созданный конструктором форм Windows
+
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label6 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label9 = new System.Windows.Forms.Label();
+			this.label10 = new System.Windows.Forms.Label();
+			this.textBox8 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox9 = new System.Windows.Forms.TextBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.button6 = new System.Windows.Forms.Button();
+			this.button7 = new System.Windows.Forms.Button();
+			this.button8 = new System.Windows.Forms.Button();
+			this.button9 = new System.Windows.Forms.Button();
+			this.button10 = new System.Windows.Forms.Button();
+			this.button11 = new System.Windows.Forms.Button();
+			this.button12 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(44, 45);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 0;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(196, 13);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Кол-во элементов в первом массива";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(13, 45);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(25, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "n = ";
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(44, 79);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.TabIndex = 3;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(13, 79);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(27, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "m = ";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(13, 114);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(265, 13);
+			this.label4.TabIndex = 5;
+			this.label4.Text = "Введите элементы первого массива через пробел";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(13, 131);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(265, 20);
+			this.textBox3.TabIndex = 6;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(328, 48);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.TabIndex = 7;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(328, 82);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.TabIndex = 8;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(153, 48);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(156, 13);
+			this.label5.TabIndex = 10;
+			this.label5.Text = "Скаляр для первого массива";
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Location = new System.Drawing.Point(153, 85);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(169, 13);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Индекс массива (через пробел)";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Location = new System.Drawing.Point(481, 13);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(195, 13);
+			this.label7.TabIndex = 12;
+			this.label7.Text = "Кол-во элемнтов во втором массиве";
+			// 
+			// textBox6
+			// 
+			this.textBox6.Location = new System.Drawing.Point(515, 45);
+			this.textBox6.Name = "textBox6";
+			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.TabIndex = 13;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Location = new System.Drawing.Point(515, 78);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(100, 20);
+			this.textBox7.TabIndex = 14;
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Location = new System.Drawing.Point(484, 45);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(25, 13);
+			this.label8.TabIndex = 15;
+			this.label8.Text = "n = ";
+			// 
+			// label9
+			// 
+			this.label9.AutoSize = true;
+			this.label9.Location = new System.Drawing.Point(484, 79);
+			this.label9.Name = "label9";
+			this.label9.Size = new System.Drawing.Size(27, 13);
+			this.label9.TabIndex = 16;
+			this.label9.Text = "m = ";
+			// 
+			// label10
+			// 
+			this.label10.AutoSize = true;
+			this.label10.Location = new System.Drawing.Point(484, 114);
+			this.label10.Name = "label10";
+			this.label10.Size = new System.Drawing.Size(264, 13);
+			this.label10.TabIndex = 17;
+			this.label10.Text = "Введите элементы второго массива через пробел";
+			// 
+			// textBox8
+			// 
+			this.textBox8.Location = new System.Drawing.Point(484, 131);
+			this.textBox8.Name = "textBox8";
+			this.textBox8.Size = new System.Drawing.Size(264, 20);
+			this.textBox8.TabIndex = 18;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(16, 331);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 19;
+			this.button1.Text = "Сброс";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox9
+			// 
+			this.textBox9.Location = new System.Drawing.Point(16, 171);
+			this.textBox9.Multiline = true;
+			this.textBox9.Name = "textBox9";
+			this.textBox9.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.textBox9.Size = new System.Drawing.Size(767, 144);
+			this.textBox9.TabIndex = 20;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(284, 129);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(158, 23);
+			this.button2.TabIndex = 21;
+			this.button2.Text = "Записать первый массив";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(755, 128);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(151, 23);
+			this.button3.TabIndex = 22;
+			this.button3.Text = "Записать второй массив";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(97, 331);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(137, 23);
+			this.button4.TabIndex = 23;
+			this.button4.Text = "Умножение на скаляр";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(240, 331);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 24;
+			this.button5.Text = "Вычесть 1";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// button6
+			// 
+			this.button6.Location = new System.Drawing.Point(321, 331);
+			this.button6.Name = "button6";
+			this.button6.Size = new System.Drawing.Size(90, 23);
+			this.button6.TabIndex = 25;
+			this.button6.Text = "Прибавить 1";
+			this.button6.UseVisualStyleBackColor = true;
+			this.button6.Click += new System.EventHandler(this.button6_Click);
+			// 
+			// button7
+			// 
+			this.button7.Location = new System.Drawing.Point(418, 330);
+			this.button7.Name = "button7";
+			this.button7.Size = new System.Drawing.Size(142, 23);
+			this.button7.TabIndex = 26;
+			this.button7.Text = "Найти число по индексу";
+			this.button7.UseVisualStyleBackColor = true;
+			this.button7.Click += new System.EventHandler(this.button7_Click);
+			// 
+			// button8
+			// 
+			this.button8.Location = new System.Drawing.Point(567, 329);
+			this.button8.Name = "button8";
+			this.button8.Size = new System.Drawing.Size(216, 23);
+			this.button8.TabIndex = 27;
+			this.button8.Text = "Конвертировать массив в двуменрный";
+			this.button8.UseVisualStyleBackColor = true;
+			this.button8.Click += new System.EventHandler(this.button8_Click);
+			// 
+			// button9
+			// 
+			this.button9.Location = new System.Drawing.Point(16, 361);
+			this.button9.Name = "button9";
+			this.button9.Size = new System.Drawing.Size(175, 23);
+			this.button9.TabIndex = 28;
+			this.button9.Text = "Проверить массив на порядок";
+			this.button9.UseVisualStyleBackColor = true;
+			this.button9.Click += new System.EventHandler(this.button9_Click);
+			// 
+			// button10
+			// 
+			this.button10.Location = new System.Drawing.Point(197, 360);
+			this.button10.Name = "button10";
+			this.button10.Size = new System.Drawing.Size(190, 23);
+			this.button10.TabIndex = 29;
+			this.button10.Text = "Сортировать массив по убыванию";
+			this.button10.UseVisualStyleBackColor = true;
+			this.button10.Click += new System.EventHandler(this.button10_Click);
+			// 
+			// button11
+			// 
+			this.button11.Location = new System.Drawing.Point(393, 358);
+			this.button11.Name = "button11";
+			this.button11.Size = new System.Drawing.Size(158, 23);
+			this.button11.TabIndex = 30;
+			this.button11.Text = "Умножить два массива";
+			this.button11.UseVisualStyleBackColor = true;
+			this.button11.Click += new System.EventHandler(this.button11_Click);
+			// 
+			// button12
+			// 
+			this.button12.Location = new System.Drawing.Point(557, 358);
+			this.button12.Name = "button12";
+			this.button12.Size = new System.Drawing.Size(208, 23);
+			this.button12.TabIndex = 31;
+			this.button12.Text = "Кол-во элементов первого массива";
+			this.button12.UseVisualStyleBackColor = true;
+			this.button12.Click += new System.EventHandler(this.button12_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1088, 450);
+			this.Controls.Add(this.button12);
+			this.Controls.Add(this.button11);
+			this.Controls.Add(this.button10);
+			this.Controls.Add(this.button9);
+			this.Controls.Add(this.button8);
+			this.Controls.Add(this.button7);
+			this.Controls.Add(this.button6);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.textBox9);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox8);
+			this.Controls.Add(this.label10);
+			this.Controls.Add(this.label9);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.textBox7);
+			this.Controls.Add(this.textBox6);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.Label label9;
+		private System.Windows.Forms.Label label10;
+		private System.Windows.Forms.TextBox textBox8;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox9;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+		private System.Windows.Forms.Button button9;
+		private System.Windows.Forms.Button button10;
+		private System.Windows.Forms.Button button11;
+		private System.Windows.Forms.Button button12;
+	}
+}
+
---
+++ Task_12_1_Forms/Form1.cs
@@ -0,0 +1,507 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_12_1_Forms
+{
+	public partial class Form1 : Form
+	{
+		Array doubleArray;
+		Array doubleArray2;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			textBox1.Text = "";
+			textBox2.Text = "";
+			textBox3.Text = "";
+			textBox4.Text = "";
+			textBox5.Text = "";
+			textBox6.Text = "";
+			textBox7.Text = "";
+			textBox8.Text = "";
+			textBox9.Text = "";
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				int n = Convert.ToInt32(textBox1.Text);
+				int m = Convert.ToInt32(textBox2.Text);
+
+				if (n < 1 || m < 1)
+				{
+					throw new Exception("\r\nКол-во элементов массива не может быть меньше одного\r\n");
+				}
+
+				doubleArray = new Array(n, m);
+				doubleArray.SetArrayElement(textBox3);
+				textBox9.Text += "\r\nМассив успешно записан\r\n";
+				doubleArray.GetArrayElement(textBox9);
+			}
+			catch (FormatException)
+			{
+				textBox9.Text += "\r\nНеверный формат\r\n";
+			}
+			catch (IndexOutOfRangeException)
+			{
+				textBox9.Text += "\r\nПроизошел выход за границы массива\r\n";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				int n = Convert.ToInt32(textBox6.Text);
+				int m = Convert.ToInt32(textBox7.Text);
+
+				if (n < 1 || m < 1)
+				{
+					throw new Exception("\r\nКол-во элементов массива не может быть меньше одного\r\n");
+				}
+
+				doubleArray2 = new Array(n, m);
+				doubleArray2.SetArrayElement(textBox8);
+				textBox9.Text += "\r\nМассив успешно записан\r\n";
+				doubleArray2.GetArrayElement(textBox9);
+			}
+			catch (FormatException)
+			{
+				textBox9.Text += "Неверный формат";
+			}
+			catch (IndexOutOfRangeException)
+			{
+				textBox9.Text += "Произошел выход за границы массива";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button4_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nвведите первый массив\r\n");
+				}
+
+				int scalar = Convert.ToInt32(textBox4.Text);
+				doubleArray.scalarArray = scalar;
+				textBox9.Text += "\r\nУмножение на скаляр: " + scalar + "\r\n";
+				doubleArray.GetArrayElement(textBox9);
+			}
+			catch (FormatException)
+			{
+				textBox9.Text += "\r\nНеверный формат\r\n";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nВведите первый массив\r\n");
+				}
+
+				doubleArray--;
+				textBox9.Text += "\r\nВычитание\r\n";
+				doubleArray.GetArrayElement(textBox9);
+			}
+			catch (FormatException)
+			{
+				textBox9.Text += "\r\nНеверный формат ввода\r\n";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button6_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nВведите первый массив\r\n");
+				}
+
+				doubleArray++;
+				textBox9.Text += "\r\nПрибавление\r\n";
+				doubleArray.GetArrayElement(textBox9);
+			}
+			catch (FormatException)
+			{
+				textBox9.Text += "\r\nНеверный формат ввода\r\n";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button7_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nвведите первый массив\r\n");
+				}
+
+				string[] str = textBox5.Text.Split(' ');
+				if (str.Length != 2)
+				{
+					throw new Exception("\r\nВ поиске по индексу должно быть всего два числа (целых)\r\n");
+				}
+
+				textBox9.Text += "\r\nЧисло по индексу: " + doubleArray[Convert.ToInt32(str[0]), Convert.ToInt32(str[1])] + "\r\n";
+			}
+			catch (FormatException)
+			{
+				textBox9.Text += "Некорректынй формат";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nвведите первый массив\r\n");
+				}
+
+				Array arrRes = (double[,])doubleArray;
+				textBox9.Text += "\r\nМассив успешно конвертирован в двумерный\r\n";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button12_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nВведите первый массив\r\n");
+				}
+
+				textBox9.Text += "\r\nКол-во элементов в первом массиве: " + doubleArray.countArray + "\r\n";
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button9_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nВведите первый массив\r\n");
+				}
+
+				if (doubleArray)
+				{
+					textBox9.Text += "\r\nМассив упорядочен по возрастанию\r\n";
+				}
+				else
+				{
+					textBox9.Text += "\r\nМассив не упорядочен по возрастанию\r\n";
+				}
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button10_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nВведите первый массив\r\n");
+				}
+
+				doubleArray.SortArrayElement();
+				textBox9.Text += "\r\nМассив был упорядочен по убыванию\r\n";
+				doubleArray.GetArrayElement(textBox9);
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+
+		private void button11_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (doubleArray == null)
+				{
+					throw new Exception("\r\nВведите первый массив\r\n");
+				}
+				
+				if (doubleArray2 == null)
+				{
+					throw new Exception("\r\nВведите второй массив\r\n");
+				}
+				Array arrayRes = doubleArray * doubleArray2;
+				textBox9.Text += "\r\nРезультат умножения двух массивов\r\n";
+				arrayRes.GetArrayElement(textBox9);
+			}
+			catch (Exception ex)
+			{
+				textBox9.Text += ex.Message;
+			}
+		}
+	}
+
+	class Array
+	{
+		private double[][] doubleArray;
+		private int n, m;
+
+		public int countArray
+		{
+			get
+			{
+				return n * m;
+			}
+		}
+		public int scalarArray
+		{
+			set
+			{
+				for (int i = 0; i < n; i++)
+				{
+					for (int j = 0; j < m; j++)
+					{
+						doubleArray[i][j] += value;
+					}
+				}
+			}
+		}
+
+		public double this[int i, int j]
+		{
+			get
+			{
+				return doubleArray[i][j];
+			}
+			set
+			{
+				doubleArray[i][j] = value;
+			}
+		}
+
+		public Array(int n, int m)
+		{
+			this.n = n;
+			this.m = m;
+			doubleArray = new double[n][];
+			for (int i = 0; i < n; i++)
+			{
+				doubleArray[i] = new double[m];
+			}
+		}
+
+		public void SetArrayElement(TextBox textBox)
+		{
+			string[] str = textBox.Text.Split(' ');
+			int index = 0;
+
+			if (str.Length != n * m)
+			{
+				throw new Exception("\r\nКол-во вводимых элементов в массив отличается от кол-ва элементов массива\r\n");
+			}
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					doubleArray[i][j] = Convert.ToDouble(str[index]);
+					index++;
+				}
+			}
+		}
+
+		public void GetArrayElement(TextBox textBox)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					textBox.Text += (doubleArray[i][j] + "\t");
+				}
+				textBox.Text += "\r\n";
+			}
+			textBox.Text += "\r\n";
+		}
+
+		public void SortArrayElement()
+		{
+			for (int i = 0; i < n; i++)
+			{
+				for (int k = 0; k < m; k++)
+				{
+					for (int j = 0; j < m - 1; j++)
+					{
+						if (doubleArray[i][j] < doubleArray[i][j + 1])
+						{
+							double temp = doubleArray[i][j];
+							doubleArray[i][j] = doubleArray[i][j + 1];
+							doubleArray[i][j + 1] = temp;
+						}
+					}
+				}
+			}
+		}
+
+		private bool checkSortArray()
+		{
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m - 1; j++)
+				{
+					if (doubleArray[i][j] > doubleArray[i][j + 1])
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		}
+
+		public static Array operator ++(Array array)
+		{
+			for (int i = 0; i < array.n; i++)
+			{
+				for (int j = 0; j < array.m; j++)
+				{
+					array.doubleArray[i][j]++;
+				}
+			}
+
+			return array;
+		}
+
+		public static Array operator --(Array array)
+		{
+			for (int i = 0; i < array.n; i++)
+			{
+				for (int j = 0; j < array.m; j++)
+				{
+					array.doubleArray[i][j]--;
+				}
+			}
+
+			return array;
+		}
+
+		public static bool operator true(Array array)
+		{
+			return array.checkSortArray();
+		}
+		public static bool operator false(Array array)
+		{
+			return !array.checkSortArray();
+		}
+
+		public static Array operator *(Array array1, Array array2)
+		{
+			if (array1.m == array2.n)
+			{
+				int n = array1.n;
+				int m = array2.m;
+				Array arrRes = new Array(n, m);
+
+				for (int i = 0; i < n; i++)
+				{
+					for (int j = 0; j < m; j++)
+					{
+						for (int k = 0; k < array1.m; k++)
+						{
+							arrRes.doubleArray[i][j] += (array1[i, k] * array2[k, j]);
+						}
+					}
+				}
+				return arrRes;
+			}
+			else
+			{
+				throw new Exception("Два массива не подходят для перемножения");
+			}
+		}
+
+		public static implicit operator double[,](Array array)
+		{
+			double[,] temp = new double[array.n, array.m];
+
+			for (int i = 0; i < array.n; i++)
+			{
+				for (int j = 0; j < array.m; j++)
+				{
+					temp[i, j] = array.doubleArray[i][j];
+				}
+			}
+
+			return temp;
+		}
+
+		public static implicit operator Array(double[,] array)
+		{
+			var n = array.GetLength(0);
+			var m = array.GetLength(1);
+			var temp = new Array(n, m);
+
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j < m; j++)
+				{
+					temp[i, j] = array[i, j];
+				}
+			}
+
+			return temp;
+		}
+	}
+}
